7f8f06b1f29e210ff7f914409bfed4cf378ac0a3
Tony Day <tony@lazylabs.com>
2019-08-04 17:41:29 +1200

    Initial implementation of simpliface

--- lib/simpliface.rb
+++ lib/simpliface.rb
@@ -1,6 +1,81 @@
-require "simpliface/version"
+# frozen_string_literal: true
 
+require 'simpliface/version'
+
+require 'active_support'
+require 'active_support/core_ext/string/inflections'
+
+# Add simple class-level interface to _service_ objects and hide the default
+# creation method (`new`) so that clients must use the exposed API.
 module Simpliface
-  class Error < StandardError; end
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  class_methods do
+    def call(*args)
+      new(*args).send(:call)
+    end
+
+    attr_writer :mandatory_arguments, :optional_arguments
+
+    def arguments(*mandatory_arguments, **optional_arguments)
+      self.mandatory_arguments = mandatory_arguments
+      self.optional_arguments = optional_arguments
+      all_argument_names.each { |name| attr_accessor name }
+    end
+
+    def mandatory_arguments
+      @mandatory_arguments ||= []
+    end
+
+    def optional_arguments
+      @optional_arguments ||= {}
+    end
+
+    def all_argument_names
+      mandatory_arguments + optional_arguments.keys
+    end
+  end
+
+  included do
+    # Prevent direct instantiation of instances (must be used via `call` API)
+    class << self
+      private :new # rubocop:disable Style/AccessModifierDeclarations
+    end
+
+    def initialize(*args, **kwargs)
+      check_no_positional_arguments(args)
+      check_no_extra_keyword_arguments(kwargs)
+      check_all_mandatory_keyword_arguments(kwargs)
+      assign_values(kwargs)
+    end
+
+    def check_no_positional_arguments(args)
+      return if args.empty?
+
+      raise ArgumentError,
+            'Positional arguments not permitted'
+    end
+
+    def check_no_extra_keyword_arguments(kwargs)
+      extras = kwargs.keys.difference(self.class.all_argument_names)
+      return if extras.empty?
+
+      raise ArgumentError,
+            "Unexpected keyword #{'argument'.pluralize(extras.count)} #{extras}"
+    end
+
+    def check_all_mandatory_keyword_arguments(kwargs)
+      missing = self.class.mandatory_arguments.difference(kwargs.keys)
+      return if missing.empty?
+
+      raise ArgumentError,
+            "Missing keyword #{'argument'.pluralize(missing.count)} #{missing}"
+    end
+
+    def assign_values(kwargs)
+      self.class.optional_arguments.merge(kwargs).each do |key, value|
+        send("#{key}=", value)
+      end
+    end
+  end
 end
--- spec/simpliface_spec.rb
+++ spec/simpliface_spec.rb
@@ -1,9 +1,71 @@
+# frozen_string_literal: true
+
 RSpec.describe Simpliface do
-  it "has a version number" do
-    expect(Simpliface::VERSION).not_to be nil
+  shared_examples 'basic specs' do
+    it 'cannot create instance directly' do
+      expect do
+        test_service.new
+      end.to raise_error(NoMethodError, /private method `new'/)
+    end
+
+    it 'raises error when called with positional argument' do
+      expect do
+        test_service.call('some arg')
+      end.to raise_error(ArgumentError, /Positional arguments not permitted/)
+    end
+
+    it 'raises error when called with unexpected keyword argument' do
+      expect do
+        test_service.call(arg3: 'val3')
+      end.to raise_error(ArgumentError, /Unexpected keyword argument \[:arg3\]/)
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context 'simple service - no arguments' do
+    let(:test_service) do
+      Class.new do
+        include Simpliface
+
+        def call
+          'a test string'
+        end
+      end
+    end
+
+    it 'works with no arguments' do
+      expect(test_service.call).to eq('a test string')
+    end
+
+    it_behaves_like 'basic specs'
+  end
+
+  context ' service with arguments' do
+    let(:test_service) do
+      Class.new do
+        include Simpliface
+
+        arguments :arg1, arg2: 'slice'
+
+        def call
+          (arg1 + arg2).upcase
+        end
+      end
+    end
+
+    it_behaves_like 'basic specs'
+
+    it 'raises error when called without mandatory keyword argument' do
+      expect do
+        test_service.call
+      end.to raise_error(ArgumentError, /Missing keyword argument \[:arg1\]/)
+    end
+
+    it 'works with mandatory argument' do
+      expect(test_service.call(arg1: 'dancer')).to eq('DANCERSLICE')
+    end
+
+    it 'pass in optional argument' do
+      expect(test_service.call(arg1: 'run', arg2: 'basil')).to eq('RUNBASIL')
+    end
   end
 end
